ca33d1fbcc6940b58207c0041dd86c171fe3f4a7
aaron <a.aa.akio.aaron@gmail.com>
2013-10-05 17:36:53 -0700

    another attempt

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var Stream = require('stream')
-var Through = require('through')
-var DuplexStream = require('duplexer')
-var DuplexEmitter = require('duplex-emitter')
+// external dependencies
 var extend = require('extend')
+var EventEmitter = require('events').EventEmitter
+// local dependencies
+var Channel = require('./lib/channel.js')
 
 module.exports = WalkieTalkie
 
@@ -26,43 +26,63 @@ WalkieTalkie.prototype.setChannels = function(channelNames) {
 
 WalkieTalkie.prototype.removeAllChannels = function() {
   var self = this
-  self.channels.map(function(channelName) {
+  for (var channelName in self.channels) {
     self.removeChannel(channelName)
-  })
+  }
 }
 
 WalkieTalkie.prototype.removeChannel = function(channelName) {
   var self = this
   // get channel
   var channel = WalkieTalkie.channels[channelName]
+  delete self.channels[channelName]
   // remove self from channel
-  removeItemFromArray(self,channel)
-  removeItemFromArray(channelName,self.channels)
-  // disconnect from channel
-  channel.map(function(subscriber) {
-    // self.outgoing.unpipe(subscriber.incomming)
-    // subscriber.outgoing.unpipe(self.incomming)
-  })
-
-  function removeItemFromArray(item,array) {
-    var index = array.indexOf(item);
-    if (index !== -1) array.splice(index, 1);
-  }
+  channel.unsubscribe(self)
 }
 
 WalkieTalkie.prototype.addChannel = function(channelName) {
   var self = this
   // get channel, create if new
-  if (!WalkieTalkie.channels[channelName]) WalkieTalkie.channels[channelName] = []
+  if (!WalkieTalkie.channels[channelName]) WalkieTalkie.channels[channelName] = new Channel({name: channelName})
   var channel = WalkieTalkie.channels[channelName]
-  // connect to channel
-  channel.map(function(subscriber) {
-    self.outgoing.pipe(subscriber.incomming)
-    subscriber.outgoing.pipe(self.incomming)
-  })
+  // add channel to subscribed channels list
+  self.channels[channelName] = {
+    _: channel,
+    name: channelName,
+    // expose an emit function that adds a ref to the emitting walkietalkie
+    emit: function emit() {
+      // normalize args array
+      var args = [].concat.apply([],arguments)
+      // add walkietalkie as first arg
+      args.unshift(self)
+      // emit event with modified args
+      this.emit.apply(this,args)
+    }.bind(channel),
+    on: function on() {
+      var emitter = self.emitter
+      emitter.on.apply(emitter,arguments)
+    },
+  }
   // add self to channel
-  channel.push(self)
-  self.channels.push(channelName)
+  channel.subscribe(self)
+}
+
+WalkieTalkie.prototype.emit = function() {
+  var self = this
+  // forward to all subscribed channels
+  for (var channelName in self.channels) {
+    var channel = self.channels[channelName]
+    channel.emit.apply(channel,arguments)
+  }
+}
+
+WalkieTalkie.prototype.on = function() {
+  var self = this
+  // forward to all subscribed channels
+  for (var channelName in self.channels) {
+    var channel = self.channels[channelName]
+    channel.on.apply(channel,arguments)
+  }
 }
 
 //
@@ -75,26 +95,12 @@ WalkieTalkie.nextId = 0
 WalkieTalkie.prototype.initialize = function(opts) {
   var self = this
   opts = opts || {}
-
   //set id
   self.id = WalkieTalkie.nextId++
-
   // create comms
-  self.incomming = Through()
-  self.outgoing = Through()
-  self.stream = DuplexStream(self.outgoing,self.incomming)
-  self.emitter = DuplexEmitter(self.stream)
-  // duplex emitters require to have been listening from the emit
-  // this is a hack to force the first emit
-  self.emitter.emit('prewarm')
-  self.incomming.write('[[]')
-
-  // expose emitter api
-  extend(self, self.emitter)
-
+  self.emitter = new EventEmitter()
   // set initial channels
-  self.channels = []
+  self.channels = {}
   var channelNames = opts.channels || opts.channel ? [opts.channel] : []
   self.setChannels(channelNames)
-
 }
